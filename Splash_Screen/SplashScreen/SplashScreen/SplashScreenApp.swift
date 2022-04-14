dd0c583d5f52c767a24c112ac6cab36077c88097
HenryVoid <chicazic@naver.com>
2022-04-14 14:12:01 +0900

    스플래시 화면 이동 방법1

---
+++ Splash_Screen/SplashScreen/SplashScreen/SplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreenApp.swift
+//  SplashScreen
+//
+//  Created by 송형욱 on 2022/04/14.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
