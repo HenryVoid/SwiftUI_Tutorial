75644ed8c20ae4ab86ff43c25b798da7aeec8f51
HenryVoid <chicazic@naver.com>
2022-04-15 20:55:45 +0900

    프로젝트 생성

---
+++ ScrollView_Offset/ScrollView_Offset/ScrollView_Offset/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScrollView_Offset
+//
+//  Created by 송형욱 on 2022/04/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScrollView_Offset/ScrollView_Offset/ScrollView_Offset/ScrollView_OffsetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollView_OffsetApp.swift
+//  ScrollView_Offset
+//
+//  Created by 송형욱 on 2022/04/15.
+//
+
+import SwiftUI
+
+@main
+struct ScrollView_OffsetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
